ddf32fa5f5ce634f8ac7fe94b1f224027e6c43ce
mashiourcse <mashiour.cseiiuc@gmail.com>
2024-02-28 01:13:57 +0600

    fixed the percentage input issue need more testing though

--- formdata.js
+++ formdata.js
@@ -1,13 +1,47 @@
-const percentageInputs = document.querySelectorAll('.nutrition');
+/* restore localstorage value to input */
+let localValue = JSON.parse(localStorage.getItem('nutritionData'));
 
-percentageInputs.forEach( input =>{
+if(localValue!==null){
+    console.log("Proceed");
+    let nutritionData = localValue.data.data;
+    let inputFields = [...document.getElementsByName('percentage')]
+    inputFields.map( (inputField, index)=>{
+        console.log(inputField);
+        inputField.setAttribute('value',nutritionData[index].value)
+        console.log(nutritionData[index]);
+    })
+    
+}else{
+    console.log("local storage is empty")
+}
+/* restore localstorage value to input */
 
-    // input.addEventListener('input', ()=>{
-        
-    // })
-})
+/* Percentage input calculation */
+var isRunning = false;
 
+[...document.getElementsByName('percentage')].forEach(function(item) {
+if (!isRunning) {
+  item.addEventListener('change', function(element) {
+   
+    const getVal = parseInt(element.target.value);
+    if (getVal <= 100) {
+      isRunning = true;
+      
+      const otherFields = Array.from(document.getElementsByName("percentage")).filter(e => e != item);
+      const change = 100 - otherFields.map(e => parseInt(e.value)).reduce((prev, curr) => prev + curr) - getVal;
+      const oldRest = 100 - (getVal + change);
+      otherFields.forEach(function(elem) {
+        const old = parseInt(elem.value);
+        elem.value = Math.round(Math.round((old + ((old / oldRest) * change)) * 100) / 100);
+      })
+      isRunning = false;
+    }
+  })
+}
+  })
+/* Percentage input calculation */
 
+/* Taking formdata and saving it to localstorage input calculation */
 document.addEventListener( 'DOMContentLoaded', ()=>{
 
     const form = document.getElementById('nutrition-form');
@@ -21,10 +55,10 @@ document.addEventListener( 'DOMContentLoaded', ()=>{
                 label: "%nutrients",
                 data: [
                     {
-                        name: "Carbs", value: document.getElementById('carbs').value
+                        name: "Proteins", value: document.getElementById('proteins').value
                     },
                     {
-                        name: "Proteins", value: document.getElementById('proteins').value
+                        name: "Carbs", value: document.getElementById('carbs').value
                     },
                     {
                         name: "Fats", value: document.getElementById('fats').value
@@ -52,3 +86,4 @@ document.addEventListener( 'DOMContentLoaded', ()=>{
 
     })
 })
+/* Taking formdata and saving it to localstorage input calculation */
